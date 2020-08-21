34078e759abd249bcf69cf04ecdea54da37bae65
carneiroRomulo <romulo.carneirodk@gmail.com>
2020-08-21 09:00:01 -0300

    Configurating database & eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,13 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
+app.use(express.json());
+
+app.get('/', (request, response) => {
   const users = [
-    {name: 'Romulo', age: 25},
-    {name: 'Lucas', age: '20'}
-  ]
+    { name: 'Romulo', age: 25 },
+    { name: 'Lucas', age: '20' },
+  ];
   return response.json(users);
 });
 
